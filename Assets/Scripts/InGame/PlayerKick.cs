63ebd52bb70ee7b75de9041bf4b741cfbfa8688d
gai99721 <kgai.yda@gmail.com>
2020-06-12 10:29:56 +0900

    [update] ロビーで自分以外のプレイヤーをキックする

--- Assets/Scripts/InGame/PlayerKick.cs
+++ Assets/Scripts/InGame/PlayerKick.cs
@@ -20,24 +20,21 @@ public class PlayerKick : MonoBehaviour
     {
         foreach (Player player in PhotonNetwork.PlayerList)
         {
-            if (!PhotonNetwork.IsMasterClient)
-            {
-                kickPlayer = player;
-            }
+
+            kickPlayer = player;
+
         }
+
         if (!PhotonNetwork.IsMasterClient)
         {
             this.gameObject.SetActive(false);
         }
     }
 
-    void OnClick()
+    public void OnClick()
     {
         if (PhotonNetwork.IsMasterClient)
         {
-            /// <summary>Request a client to disconnect (KICK). Only the master client can do this</summary>
-            /// <remarks>Only the target player gets this event. That player will disconnect automatically, which is what the others will notice, too.</remarks>
-            /// <param name="kickPlayer">The Player to kick.</param>
             PhotonNetwork.CloseConnection(kickPlayer);
         }
     }
