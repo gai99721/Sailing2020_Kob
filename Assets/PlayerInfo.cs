bead9a3051b21422656504f193ae519301c32731
gai99721 <kgai.yda@gmail.com>
2021-01-18 07:59:56 +0900

    [add]serverpro

---
+++ Assets/PlayerInfo.cs
@@ -0,0 +1,37 @@
+﻿/*クラス名：PlayerInfo.cs
+ * 作成日：01/08
+ * 作成者：小林凱
+ */
+
+using Common;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Sailing.Server
+{
+    public class PlayerInfo : MonoBehaviour
+    {
+
+        [SerializeField]
+        private Text userNameText;
+        //private Text userBirthText;
+        //private Text userPrefText;
+
+        private void Start()
+        {
+
+            userNameText = ObjectFind.ChildFind("UserData", ObjectFind.ChildFind("Name", transform)).gameObject.GetComponent<Text>();
+
+            GetUserData();
+
+        }
+
+        private void GetUserData()
+        {
+
+            userNameText.text = PlayerPrefs.GetString(UserDataKey.UserName_Key);
+
+        }
+    }
+}
